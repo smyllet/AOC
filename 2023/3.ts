fe359db38b5585e1e564f777ea07ecabbd2bf75a
Bryan <bryan.prolong@outlook.fr>
2023-12-03 11:14:10 +0100

    AOC 2023/12/03 Part 2

--- 2023/3.ts
+++ 2023/3.ts
@@ -7,6 +7,7 @@ const input = await fs.promises.readFile("./input.txt", {
 const splitInput = input.split("\n");
 
 const validNumbers: number[] = [];
+const gearParts: { id: string; ratio: number[] }[] = [];
 
 splitInput.forEach((line, lIndex) => {
   let currentValue = "";
@@ -20,22 +21,35 @@ splitInput.forEach((line, lIndex) => {
       currentValue.length > 0
     ) {
       let isValid = false;
+      const number = Number.parseInt(currentValue);
 
       // Check adjacent symbol
+      const startExtract = Math.max(cIndex - currentValue.length - 1, 0);
       for (
         let i = Math.max(lIndex - 1, 0);
         i <= Math.min(lIndex + 1, splitInput.length - 1);
         i++
       ) {
-        let extract = splitInput[i].substring(
-          Math.max(cIndex - currentValue.length - 1, 0),
-          cIndex + 1,
-        );
+        let extract = splitInput[i].substring(startExtract, cIndex + 1);
         if (extract.match(/[^(\d|\.)]/)) isValid = true;
+
+        const gear = extract.indexOf("*");
+
+        if (gear > -1) {
+          let id = `${i}-${startExtract + gear}`;
+          let gFind = gearParts.find((g) => g.id == id);
+          if (gFind) gFind.ratio.push(number);
+          else {
+            gearParts.push({
+              id: id,
+              ratio: [number],
+            });
+          }
+        }
       }
 
       if (isValid) {
-        validNumbers.push(Number.parseInt(currentValue));
+        validNumbers.push(number);
       }
 
       currentValue = "";
@@ -44,7 +58,18 @@ splitInput.forEach((line, lIndex) => {
 });
 
 console.log(
+  "Part 1 :",
   validNumbers.reduce((a, b) => {
     return a + b;
   }),
+);
+
+console.log(
+  "Part 2 :",
+  gearParts
+    .filter((gear) => gear.ratio.length === 2)
+    .flatMap((gear) => gear.ratio[0] * gear.ratio[1])
+    .reduce((a, b) => {
+      return a + b;
+    }),
 );
